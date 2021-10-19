6adda6af23ca1a159c27845e6c5665258c15bc39
coder m16 <72678773+pro-m16@users.noreply.github.com>
2021-10-18 19:50:30 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+ function openPage()
+               {
+                   var x = document.getElementById("facul").value;
+                   
+                   if (x === "SCIT"){
+                       window.open("scit.html");
+                   }
+
+                    if (x === "FENC") {
+                       window.open("fenc.html");
+                   }
+
+                    if (x === "COBAM") {
+                       window.open("cobam.html");
+                   }
+
+                    if (x === "COHS") {
+                       window.open("cohs.html");
+                   }
+
+                    if (x === "FOBE") {
+                       window.open("fobe.html");
+                   }
+
+                    if (x === "FELS") {
+                       window.open("scit.html");
+                   }
+
+                    if (x === "FOSS") {
+                       window.open("scit.html");
+                   }
+
+                    if (x === "FOL") {
+                       window.open("scit.html");
+                   }
+               }
